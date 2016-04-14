6ffab9cb6ea6951dffe53b1aab268dfb92483b3a
Aswin P J <aswinpj@outlook.com>
2016-04-14 10:05:56 -0700

    Added option for rit init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate argparse;
+
+use argparse::{ArgumentParser, StoreTrue, Store};
+
 fn main() {
-    println!("Hello, world!");
+    let mut initialise = false;
+
+    {  // this block limits scope of borrows by ap.refer() method
+        let mut ap = ArgumentParser::new();
+        ap.set_description("rit - git written in Rust!");
+        ap.refer(&mut initialise)
+            .add_option(&["-i", "--initialise"], StoreTrue,
+            "initialise  repository");
+
+        ap.parse_args_or_exit();
+    }
+
+    if initialise {
+        println!("Initialising empty repository");
+    }
+
 }
